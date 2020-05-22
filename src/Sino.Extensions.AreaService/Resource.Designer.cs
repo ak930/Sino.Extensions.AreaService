8c862fb259912333e3020d1cc56d438784032fe9
liyu <1121294648@qq.com>
2020-05-22 16:08:24 +0800

    1.6版本更新

--- src/Sino.Extensions.AreaService/Resource.Designer.cs
+++ src/Sino.Extensions.AreaService/Resource.Designer.cs
@@ -10,6 +10,7 @@
 
 namespace Sino.Extensions.AreaService {
     using System;
+    using System.Reflection;
     
     
     /// <summary>
@@ -39,7 +40,7 @@ namespace Sino.Extensions.AreaService {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Sino.Extensions.AreaService.Resource", typeof(Resource).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Sino.Extensions.AreaService.Resource", typeof(Resource).GetTypeInfo().Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
